65471abf97d5dcf7afa29fbc4d86e9073051fcc7
Sylvester Keil <sylvester@keil.or.at>
2011-07-05 09:36:35 +0200

    initial commit

---
+++ lib/latex/decode.rb
@@ -0,0 +1,10 @@
+
+require 'latex/decode/version'
+
+module LaTeX
+  class << self
+    def decode (string)
+      string
+    end
+  end
+end
---
+++ lib/latex/decode/version.rb
@@ -0,0 +1,5 @@
+module LaTeX
+  class Decode
+    VERSION = '0.0.1'.freeze
+  end
+end

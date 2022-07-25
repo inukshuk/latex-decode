ca64c857c9c99bc50193cd343ce126ceba8c7a45
Sylvester Keil <sylvester@keil.or.at>
2022-07-25 14:49:58 +0200

    Update and simplify dev dependencies

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,5 @@
 begin
-  case
-  when defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
-    require 'rubinius/debugger'
-  when RUBY_VERSION > '2.0'
-    require 'byebug'
-  else
-    require 'debugger'
-  end
+  require 'debug'
 rescue LoadError
   # ignore
 end
